7f79fe0c0604ded1b92bd726897a1defd6b630b8
Samir Shrestha <samir.shrestha92@gmail.com>
2021-02-20 13:59:50 -0600

    Finished PostActivity Layout.

---
+++ app/src/main/java/org/samir/universitybazaar/PostActivity.java
@@ -0,0 +1,16 @@
+package org.samir.universitybazaar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.samir.universitybazaar.R;
+
+public class PostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+    }
+}
