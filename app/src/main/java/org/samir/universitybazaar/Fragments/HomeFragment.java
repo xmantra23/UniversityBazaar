a83a53e8a3a0499e487f909544393676652ec641
YiFei <lyf14037019@163.com>
2021-03-18 01:28:50 +0800

    advertisement ok! it switch to next picture after 5s click it can open the Browser for link

--- app/src/main/java/org/samir/universitybazaar/Fragments/HomeFragment.java
+++ app/src/main/java/org/samir/universitybazaar/Fragments/HomeFragment.java
@@ -1,28 +1,38 @@
 package org.samir.universitybazaar.Fragments;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import org.samir.universitybazaar.Activity.HomeActivity;
 import org.samir.universitybazaar.Activity.Clubs.MyClubsActivity;
 import org.samir.universitybazaar.Activity.Clubs.MySubscriptionsActivity;
+import org.samir.universitybazaar.Adapter.AdvertisementAdapter;
 import org.samir.universitybazaar.Database.UserSession;
 import org.samir.universitybazaar.Activity.Posts.MyPostsActivity;
 import org.samir.universitybazaar.Activity.Profile.ViewProfileActivity;
 import org.samir.universitybazaar.R;
 import org.samir.universitybazaar.Utility.Constants;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Samir Shrestha
@@ -35,6 +45,35 @@ public class HomeFragment extends Fragment {
     private BottomNavigationView bottomNavigationView;
     private ImageView manageClubArrow,manageSubArrow,managePostArrow,manageSaleArrow,managePurchaseArrow,manageLoanArrow;
     private TextView txtProfile,txtLogout,txtMessages;
+
+    /**
+     * @author YiFei Lu
+     */
+    private ViewPager advertisement;
+    private LinearLayout point;
+    private List<View> viewList;
+    private AdvertisementAdapter adAdapter;
+    private List<ImageView> pointList;
+    private Object[][] picIds = {
+            {R.mipmap.kfc,"https://www.kfc.com/"},
+            {R.mipmap.sky,"https://thatgamecompany.com/sky/"},
+            {R.mipmap.apple,"https://www.apple.com/"},
+            {R.mipmap.piza,"https://www.pizzahut.com/index.php#/home"},
+            {R.mipmap.advertisement,"null"}
+    };
+
+    //Timer that can help advertisement move itself
+    @SuppressLint("HandlerLeak")
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg){
+            if(msg.what == 1){
+                int current_item = advertisement.getCurrentItem();
+                advertisement.setCurrentItem(current_item+1);
+                handler.sendEmptyMessageDelayed(1,5000);
+            }
+        }
+    };
 
     @Nullable
     @Override
@@ -103,6 +142,39 @@ public class HomeFragment extends Fragment {
         txtProfile = view.findViewById(R.id.txtProfile);
         txtLogout = view.findViewById(R.id.txtLogout);
         txtMessages = view.findViewById(R.id.txtMessages);
+        advertisement = view.findViewById(R.id.advertisement);
+        point = view.findViewById(R.id.point);
+
+        /**
+         * @author:YIfei Lu
+         * */
+        //init the point that index which picture is showing
+        pointList = new ArrayList<>();
+        //init the Viewpager's views
+        viewList = new ArrayList<>();
+        for(int i=0;i<picIds.length;i++){
+            View adView = LayoutInflater.from(getActivity()).inflate(R.layout.item_advertisement,null);
+            ImageView iv = adView.findViewById(R.id.item_advertisement);
+            iv.setImageResource((Integer) picIds[i][0]);
+            String website = (String) picIds[i][1];
+            iv.setOnClickListener((ImageView)->{handleImageListener(website);});
+            viewList.add(adView);
+            //point
+            ImageView pointIv = new ImageView(getActivity());
+            LinearLayout.LayoutParams lp=new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+            lp.setMargins(0,0,20,0);
+            pointIv.setLayoutParams(lp);
+            pointIv.setImageResource(R.mipmap.no);
+            pointList.add(pointIv);
+            point.addView(pointIv);
+        }
+        pointList.get(0).setImageResource(R.mipmap.yes);//set the first point as select
+
+        adAdapter = new AdvertisementAdapter(viewList);
+        advertisement.setAdapter(adAdapter);
+
+        handler.sendEmptyMessageDelayed(1,5000);
+        setVPListener();
     }
 
     //handles the bottom navigation view icon presses.
@@ -137,5 +209,39 @@ public class HomeFragment extends Fragment {
             }
             return false;
         });
+    }
+
+    /**
+     * @author :Yifei Lu
+     * */
+    private void setVPListener(){
+        //point listener
+        advertisement.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                for(int i=0;i<pointList.size();i++){
+                    pointList.get(i).setImageResource(R.mipmap.no);
+                }
+                pointList.get(position%pointList.size()).setImageResource(R.mipmap.yes);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+    }
+
+    private void handleImageListener(String url){
+        //the ImageView's Listener that can link to the url provided
+        if(!url.equals("null")){
+            Uri uri = Uri.parse(url);
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
+        }
+
     }
 }
