de6eccc54f90620fc8b5905d4bcdbdafb2ce7e8f
Samir Shrestha <samir.shrestha92@gmail.com>
2021-04-20 13:13:48 -0500

    search by title finished.

--- app/src/main/java/org/samir/universitybazaar/Activity/Search/SearchPostsActivity.java
+++ app/src/main/java/org/samir/universitybazaar/Activity/Search/SearchPostsActivity.java
@@ -1,63 +1,87 @@
 package org.samir.universitybazaar.Activity.Search;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import org.samir.universitybazaar.Adapter.MyPostAdapter;
+import org.samir.universitybazaar.Database.PostDAO;
+import org.samir.universitybazaar.Database.UserSession;
+import org.samir.universitybazaar.Models.Post;
+import org.samir.universitybazaar.Models.User;
 import org.samir.universitybazaar.R;
+
+import java.util.ArrayList;
 
 public class SearchPostsActivity extends AppCompatActivity {
     private EditText edtTxtSearch;
     private RecyclerView postRecView;
+    private Button btnSearch;
     private MyPostAdapter adapter;
+    private UserSession session;
+    private PostDAO dao;
+    private RadioGroup searchTypeRadioGroup;
+    private ArrayList<Post> posts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_posts);
+
+        session = new UserSession(this);
+        User user = session.isUserLoggedIn();
+        dao = new PostDAO(this);
         initViews();
+
+        if(user != null){
+            initRecView();
+        }
     }
 
     private void initViews() {
         edtTxtSearch = findViewById(R.id.edtTxtSearch);
         postRecView = findViewById(R.id.postRecView);
+        btnSearch = findViewById(R.id.btnSearch);
         adapter = new MyPostAdapter(this);
+        searchTypeRadioGroup = findViewById(R.id.searchTypeRadioGroup);
     }
 
-    public void onRadioButtonClicked(View view){
-        boolean checked = ((RadioButton) view).isChecked();
+    private void initRecView() {
+        posts = dao.getAllPosts();
+        adapter.setPosts(posts);
+        postRecView.setAdapter(adapter);
+        postRecView.setLayoutManager(new LinearLayoutManager(this,RecyclerView.VERTICAL,true));
+    }
 
-        switch(view.getId()){
+
+
+    public void onSearchButtonClicked(View view){
+        String searchTerm = edtTxtSearch.getText().toString().trim();
+        switch(searchTypeRadioGroup.getCheckedRadioButtonId()){
             case R.id.radioTitle:
-                if(checked){
-                    String searchTerm = edtTxtSearch.getText().toString();
-                    Log.d("SearchPostActivity",searchTerm);
-                }
+                posts = dao.getPostByTitle(searchTerm);
+                adapter.setPosts(posts);
+                postRecView.setAdapter(adapter);
+                postRecView.setLayoutManager(new LinearLayoutManager(this,RecyclerView.VERTICAL,true));
                 break;
             case R.id.radioDescription:
-                if(checked){
-                    String searchTerm = edtTxtSearch.getText().toString();
-                    Log.d("SearchPostActivity",searchTerm);
-                }
+                Log.d("SearchPostActivity",searchTerm);
                 break;
             case R.id.radioDate:
-                if(checked){
-                    String searchTerm = edtTxtSearch.getText().toString();
-                    Log.d("SearchPostActivity",searchTerm);
-                }
+                Log.d("SearchPostActivity",searchTerm);
                 break;
             case R.id.radioAuthor:
-                if(checked){
-                    String searchTerm = edtTxtSearch.getText().toString();
-                    Log.d("SearchPostActivity",searchTerm);
-                }
-                break;
+                Log.d("SearchPostActivity",searchTerm);
         }
     }
+
 }
--- app/src/main/java/org/samir/universitybazaar/Database/PostDAO.java
+++ app/src/main/java/org/samir/universitybazaar/Database/PostDAO.java
@@ -2,11 +2,14 @@ package org.samir.universitybazaar.Database;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 
 import org.samir.universitybazaar.Models.Post;
 import org.samir.universitybazaar.Models.Profile;
+
+import java.util.ArrayList;
 
 public class PostDAO {
     private DatabaseHelper dbHelper;
@@ -66,6 +69,137 @@ public class PostDAO {
         }catch(SQLException e) {
             e.printStackTrace();
             return false;
+        }
+    }
+
+
+    /**
+     * @author Samir Shrestha
+     */
+    public ArrayList<Post> getPostByTitle(String title){
+        ArrayList<Post> posts = new ArrayList<>();
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        try {
+            String[] columns = new String[]{
+                    "_id",
+                    "title",
+                    "description",
+                    "creatorId",
+                    "creatorName",
+                    "createdDate",
+                    "category",
+            };
+
+            String[] args = new String[]{title+"%"};
+
+            //retrieve all the posts from the posts table where title == provided title in the argument
+            Cursor cursor = db.query("posts", columns, "title LIKE ?", args, null, null, null);
+            if(cursor != null){
+                if(cursor.moveToFirst()){
+                    boolean isLast = false;
+                    while(!isLast){ // continues until all the retrieved rows have been iterated.
+                        Post post = new Post();
+                        int postId = cursor.getInt(cursor.getColumnIndex("_id"));
+                        String description = cursor.getString(cursor.getColumnIndex("description"));
+                        String creatorId = cursor.getString(cursor.getColumnIndex("creatorId"));
+                        String creatorName = cursor.getString(cursor.getColumnIndex("creatorName"));
+                        String createdDate = cursor.getString(cursor.getColumnIndex("createdDate"));
+                        String category = cursor.getString(cursor.getColumnIndex("category"));
+                        title = cursor.getString(cursor.getColumnIndex("title"));
+
+                        post.setId(postId);
+                        post.setTitle(title);
+                        post.setDescription(description);
+                        post.setCreatorId(creatorId);
+                        post.setCreatorName(creatorName);
+                        post.setCreatedDate(createdDate);
+                        post.setCategory(category);
+
+                        posts.add(post); //add the post to the posts arraylist.
+
+                        if(cursor.isLast()){ // we are at the last row of the dataset. no need to continue anymore.
+                            isLast = true;
+                        }else{
+                            cursor.moveToNext(); //move to the next row in the dataset.
+                        }
+                    }
+                }
+                db.close();
+                cursor.close();
+                return posts; //return all the posts we got for that memberId in the posts table.
+            }else{
+                db.close();
+                cursor.close();
+                return null;
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+            db.close();
+            return null;
+        }
+    }
+
+    /**
+     * @author Samir Shrestha
+     * @description This method returns all the posts from all users.
+     */
+    public ArrayList<Post> getAllPosts(){
+        ArrayList<Post> posts = new ArrayList<>();
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        try {
+            String[] columns = new String[]{
+                    "_id",
+                    "title",
+                    "description",
+                    "creatorId",
+                    "creatorName",
+                    "createdDate",
+                    "category",
+            };
+
+            //retrieve all the post in the posts table.
+            Cursor cursor = db.query("posts", columns, null, null, null, null, null);
+            if(cursor != null){
+                if(cursor.moveToFirst()){
+                    boolean isLast = false;
+                    while(!isLast){ //continue until there are no more rows to process in the dataset.
+                        Post post = new Post();
+                        int postId = cursor.getInt(cursor.getColumnIndex("_id"));
+                        String title = cursor.getString(cursor.getColumnIndex("title"));
+                        String description = cursor.getString(cursor.getColumnIndex("description"));
+                        String creatorId = cursor.getString(cursor.getColumnIndex("creatorId"));
+                        String creatorName = cursor.getString(cursor.getColumnIndex("creatorName"));
+                        String createdDate = cursor.getString(cursor.getColumnIndex("createdDate"));
+                        String category = cursor.getString(cursor.getColumnIndex("category"));
+
+                        post.setId(postId);
+                        post.setTitle(title);
+                        post.setDescription(description);
+                        post.setCreatorId(creatorId);
+                        post.setCreatorName(creatorName);
+                        post.setCreatedDate(createdDate);
+                        post.setCategory(category);
+
+                        posts.add(post);
+                        if(cursor.isLast()){
+                            isLast = true;
+                        }else{
+                            cursor.moveToNext();
+                        }
+                    }
+                }
+                db.close();
+                cursor.close();
+                return posts;
+            }else{
+                db.close();
+                cursor.close();
+                return null;
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+            db.close();
+            return null;
         }
     }
 }
